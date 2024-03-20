bbd92e1d26b7c8791ce41d50192c3ba7ee695e12
AdrianFurquim <adrian.furquim26@gmail.com>
2024-03-20 16:07:15 -0300

    Ajustando Catalogo do Vita Bloom produtos

--- src/assets/components/pages/Catalogo.jsx
+++ src/assets/components/pages/Catalogo.jsx
@@ -1,11 +1,20 @@
 import Produto from "../layout/Produto"
 import styles from "./Catalogo.module.css"
-import produtoUva1 from "../../img/produto_uva_1.png"
-import produtoUva2 from "../../img/produto_uva_2.png"
-import produtoMelancia1 from "../../img/produto_melancia_1.png"
-import produtoAbacaxi1 from "../../img/produto_abacaxi_1.png"
-import produtoLaranja1 from "../../img/produto_laranja_1.png"
-import produtoMorango1 from "../../img/produto_morango_1.png"
+
+import cremeUvaDivno from "../../img/produto_uva_1.png"
+import essenciaUvaCelestial from "../../img/produto_uva_2.png"
+import abacaxiOlimpus from "../../img/produto_abacaxi_2.png"
+import avocadoGlow from "../../img/produto_abacate_1.png"
+import pineappleDeling from "../../img/produto_abacaxi_1.png"
+import tropicalBreeze from "../../img/produto_abacaxi_2.png"
+import bananaBliss from "../../img/produto_banana_1.png"
+import citrusBurst from "../../img/produto_laranja_1.png"
+import orangeRadiano from "../../img/produto_laranja_2.png"
+import sunriseCitrus from "../../img/produto_laranja_4.png"
+import greenAplleRevive from "../../img/produto_maca_1.png"
+import strawberryKiss from "../../img/produto_morango_1.png"
+import berryFreshness from "../../img/produto_morango_5.png"
+
 
 import { useState, useEffect } from "react"
 
@@ -32,23 +41,56 @@ export default function Catalogo(){
         }, 100);
     }, []);
 
+    // Função para obter a imagem com base no ID do produto
+    const getImagemProduto = (id) => {
+        switch (id) {
+            case 2:
+                return cremeUvaDivno;
+            case 3:
+                return essenciaUvaCelestial;
+            case 4:
+                return abacaxiOlimpus;
+            case 10:
+                return avocadoGlow;
+            case 11:
+                return pineappleDeling;
+            case 12:
+                return tropicalBreeze;
+            case 13:
+                return bananaBliss;
+            case 14:
+                return citrusBurst;
+            case 15:
+                return orangeRadiano;
+            case 16:
+                return sunriseCitrus;
+            case 17:
+                return greenAplleRevive;
+            case 18:
+                return strawberryKiss;
+            case 19:
+                return berryFreshness;
+            default:
+                return ; // Retorna null se o ID não corresponder a nenhuma imagem
+        }
+    };
 
-    let texto = "Apresentando 'Essência Uva Celestial': uma fragrância que captura toda a exuberância e frescor das vinhas exuberantes em uma única gota. Com uma mistura encantadora de notas frutadas e florais, esta fragrância evoca a sensação celestial de um pomar de uvas em plena floração. Clike no carrinho para adicionar este produto em seu carrinho."
-    return(
-        <div className={styles.produtos_container}> 
-            <div  className={styles.teste}>
-                {produtos.map(produto => (
-                    <div key={produto.id}>
-                        <Produto className={styles.teste} nome={produto.nomeProduto} valor={produto.valorProduto} descricao={produto.descricaoProduto} imagem={produtoAbacaxi1}/>
-                        <Produto className={styles.teste} nome={produto.nomeProduto} valor={produto.valorProduto} descricao={produto.descricaoProduto} imagem={produtoAbacaxi1}/>
-                        <Produto className={styles.teste} nome={produto.nomeProduto} valor={produto.valorProduto} descricao={produto.descricaoProduto} imagem={produtoAbacaxi1}/>
-                        <Produto className={styles.teste} nome={produto.nomeProduto} valor={produto.valorProduto} descricao={produto.descricaoProduto} imagem={produtoAbacaxi1}/>
-                        <Produto className={styles.teste} nome={produto.nomeProduto} valor={produto.valorProduto} descricao={produto.descricaoProduto} imagem={produtoAbacaxi1}/>
-                        <Produto className={styles.teste} nome={produto.nomeProduto} valor={produto.valorProduto} descricao={produto.descricaoProduto} imagem={produtoAbacaxi1}/>
-                        <Produto className={styles.teste} nome={produto.nomeProduto} valor={produto.valorProduto} descricao={produto.descricaoProduto} imagem={produtoAbacaxi1}/>
-                    </div>
-                ))}
-            </div>
+    return (
+        <div className={styles.produtos_container}>
+          <div className={styles.teste}>
+            {produtos.map(produto => (
+              <div key={produto.id}>
+                <div className={styles.outroteste}>
+                <Produto
+                  nome={produto.nomeProduto}
+                  valor={produto.valorProduto}
+                  descricao={produto.descricaoProduto}
+                  imagem={getImagemProduto(produto.idProduto)} // Use a função para obter a imagem com base no ID do produto
+                />
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-    )
+      );
 }
